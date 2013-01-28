d6ac3e4fd84129ad325dff825bbb936ae975da7f
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2013-01-28 06:25:19 +0300

    Added dummy plugins for Perl::Critic and Perl::Tidy support

---
+++ lib/Farabi/Plugin/PerlCritic.pm
@@ -0,0 +1,27 @@
+package Farabi::Plugin::PerlCritic;
+
+our $VERSION = '0.28';
+
+sub new {
+	return bless {};
+}
+
+# Returns the plugin's name
+sub plugin_name {
+	return 'Perl::Critic support';
+}
+
+1;
+
+__END__
+
+=pod
+s
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012-2013 by Ahmad M. Zawawi
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
