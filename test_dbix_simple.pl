c4b689fc99ea4b08f26f5cad51f7a0bbcbe1e496
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2013-02-01 05:59:38 +0300

    First proof of concept of using SQLite and DBIx::Simple to build Farabi DB

---
+++ test_dbix_simple.pl
@@ -0,0 +1,28 @@
+use Modern::Perl;
+use DBIx::Simple;
+
+my $db = DBIx::Simple->connect('dbi:SQLite:dbname=farabi.dat');
+
+my $list;
+eval { $list = $db->query("SELECT * FROM recent_list") };
+
+if ($@) {
+	say "Creating table...";
+	$db->query(<<SQL);
+CREATE TABLE recent_list (
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	name TEXT
+)
+SQL
+	say "Table created";
+}
+else {
+
+	for my $row ( $db->query('SELECT id, name FROM recent_list')->hashes ) {
+		say "Id: $row->{id}, Name: $row->{name}";
+	}
+	$db->query( "INSERT INTO recent_list(name) VALUES(?)", qw(Ahmad) );
+}
+
+$db->disconnect;
+
