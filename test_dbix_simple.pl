0c935a884d1b280a20f7e87cdf952e8f0df982e7
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2013-02-01 06:06:43 +0300

    Better syntax...

--- test_dbix_simple.pl
+++ test_dbix_simple.pl
@@ -3,25 +3,18 @@ use DBIx::Simple;
 
 my $db = DBIx::Simple->connect('dbi:SQLite:dbname=farabi.db');
 
-eval { $db->query("SELECT * FROM recent_list") };
-
-if ($@) {
-	say "Creating table...";
-	$db->query(<<SQL);
-CREATE TABLE recent_list (
+# DDL...
+$db->query(<<SQL);
+CREATE TABLE IF NOT EXISTS recent_list (
 	id INTEGER PRIMARY KEY AUTOINCREMENT, 
 	name TEXT
 )
 SQL
-	say "Table created";
-}
-else {
 
-	for my $row ( $db->query('SELECT id, name FROM recent_list')->hashes ) {
-		say "Id: $row->{id}, Name: $row->{name}";
-	}
-	$db->query( "INSERT INTO recent_list(name) VALUES(?)", qw(Ahmad) );
+for my $row ( $db->query('SELECT id, name FROM recent_list')->hashes ) {
+	say "Id: $row->{id}, Name: $row->{name}";
 }
+$db->query( "INSERT INTO recent_list(name) VALUES(?)", qw(Ahmad) );
 
 $db->disconnect;
 
