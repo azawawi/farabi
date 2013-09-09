1a00a4a164178bd62192951dbaeda3bfcee8b2f8
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2013-09-09 05:58:43 +0300

    Moved functionality from ../test_system to this full blown Dancer application

--- TestApp/bin/app.pl
+++ TestApp/bin/app.pl
@@ -1,4 +1,24 @@
 #!/usr/bin/env perl
 use Dancer;
 use TestApp;
-dance;
+use EV ();
+use Twiggy::Server ();
+
+# Create a twiggy AnyEvent server
+my $server = Twiggy::Server->new(
+	host => '127.0.0.1',
+	port => 5000,
+);
+
+# Define our dancer application
+my $app = sub {
+	my $env = shift;
+	Dancer->dance( Dancer::Request->new( env => $env ) );
+};
+
+# The dancer application is run by twiggy
+$server->register_service($app);
+
+# Now we run the event loop
+EV::loop;
+
--- TestApp/lib/TestApp.pm
+++ TestApp/lib/TestApp.pm
@@ -1,10 +1,149 @@
 package TestApp;
 use Dancer ':syntax';
 
+
+use Modern::Perl;
+use File::Temp ();
+use EV;
+use AnyEvent ();
+use Data::Printer;
+use IO::All;
+use Dancer;
+use Dancer::Plugin::WebSocket;
+
 our $VERSION = '0.1';
 
+my $pid;
+my %timers  = ();
+my %results = ();
+
+my $IS_WIN32 = $^O eq 'MSWin32';
+
+my %cmds;
+if($IS_WIN32) {
+%cmds = (
+	'0' => 'dir',
+	'1' => 'dir -B',
+	'2' => 'dir -D',
+);
+} else {
+ %cmds = (
+
+	'0' => 'ls',
+	'1' => 'ls -a',
+	'2' => 'ls -al',
+);
+}
+
 get '/' => sub {
-    template 'index';
+	return <<HTML;
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+	width: 100%;
+	border: 1px solid black;
+}
+iframe {
+	border: 1px solid black;
+	width: 100%;
+	height: 500px;
+}
+</style>
+<script>
+
+
+function start() {
+	document.getElementById("out1").src = "/run/0";
+	document.getElementById("out2").src = "/run/1";
+	document.getElementById("out3").src = "/run/2";
+	
+	var interval = setInterval(function() {
+		document.getElementById("out1").src = "/result/0";
+		document.getElementById("out2").src = "/result/1";
+		document.getElementById("out3").src = "/result/2";
+	}, 2000);
+}
+</script>
+</head>
+<body>
+<button id="start" onClick="start()">Start!</button>
+<table>
+	<tr>
+		<td><iframe id="out1"></iframe></td>
+		<td><iframe id="out2"></iframe></td>
+		<td><iframe id="out3"></iframe></td>
+	</tr>
+</table>
+</body>
+</html>
+HTML
+};
+
+get '/run/:cmd_id' => sub {
+	my $cmd_id = param('cmd_id');
+	my $cmd = $cmds{$cmd_id} or return "Invalid $cmd_id";
+
+	my $stdout_fh    = File::Temp->new;
+	my $stderr_fh    = File::Temp->new;
+	my $stdout_fname = $stdout_fh->filename;
+	my $stderr_fname = $stderr_fh->filename;
+	$stdout_fh->unlink_on_destroy(0);
+	$stderr_fh->unlink_on_destroy(0);
+	$stdout_fh->close;
+	$stderr_fh->close;
+
+	my @cmd = ("$cmd >$stdout_fname 2>$stderr_fname");
+
+	if ( $^O eq 'MSWin32' ) {
+
+		# Windows OS
+		$pid = system( 1, @cmd );
+	}
+	else {
+		# Non-windows OS
+		$pid = fork();
+		if ( !$pid ) {
+			exec(@cmd);
+			exit;
+		}
+	}
+	
+	# Make sure the old result is dead
+	delete $results{$cmd_id};
+
+	my $timer;
+	$timer = AnyEvent->timer(
+		after    => 0,
+		interval => 0.5,
+		cb       => sub {
+			my $stdout = io($stdout_fname)->slurp;
+			my $stderr = io($stderr_fname)->slurp;
+
+			# Update the process result record
+			$results{$cmd_id} = [ $pid, $stdout, $stderr ];
+
+			my $running = kill 0, $pid;
+			# Cancel timer if process is dead
+			delete $timers{$timer} unless ($running);
+		}
+	);
+	$timers{$timer} = $timer;
+
+	return "Running `$cmd`";
+
+};
+
+get '/result/:index' => sub {
+	my $index = param('index');
+
+	return "Invalid index" unless defined $index;
+
+	my $result = $results{$index};
+	
+	return "No result for `$index`" unless defined $result;
+	return '<pre>' . $result->[1] . $result->[2] . '</pre>';
 };
 
 true;
