3440f09f5ef6ffd18296b2fae3e38489915dd4c4
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2013-09-09 06:00:15 +0300

    Perl tidy...

--- TestApp/bin/app.pl
+++ TestApp/bin/app.pl
@@ -1,19 +1,19 @@
 #!/usr/bin/env perl
 use Dancer;
 use TestApp;
-use EV ();
+use EV             ();
 use Twiggy::Server ();
 
 # Create a twiggy AnyEvent server
 my $server = Twiggy::Server->new(
-	host => '127.0.0.1',
-	port => 5000,
+    host => '127.0.0.1',
+    port => 5000,
 );
 
 # Define our dancer application
 my $app = sub {
-	my $env = shift;
-	Dancer->dance( Dancer::Request->new( env => $env ) );
+    my $env = shift;
+    Dancer->dance( Dancer::Request->new( env => $env ) );
 };
 
 # The dancer application is run by twiggy
--- TestApp/lib/TestApp.pm
+++ TestApp/lib/TestApp.pm
@@ -1,7 +1,6 @@
 package TestApp;
 use Dancer ':syntax';
 
-
 use Modern::Perl;
 use File::Temp ();
 use EV;
@@ -20,23 +19,24 @@ my %results = ();
 my $IS_WIN32 = $^O eq 'MSWin32';
 
 my %cmds;
-if($IS_WIN32) {
-%cmds = (
-	'0' => 'dir',
-	'1' => 'dir -B',
-	'2' => 'dir -D',
-);
-} else {
- %cmds = (
+if ($IS_WIN32) {
+    %cmds = (
+        '0' => 'dir',
+        '1' => 'dir -B',
+        '2' => 'dir -D',
+    );
+}
+else {
+    %cmds = (
 
-	'0' => 'ls',
-	'1' => 'ls -a',
-	'2' => 'ls -al',
-);
+        '0' => 'ls',
+        '1' => 'ls -a',
+        '2' => 'ls -al',
+    );
 }
 
 get '/' => sub {
-	return <<HTML;
+    return <<HTML;
 <!DOCTYPE html>
 <html>
 <head>
@@ -82,68 +82,69 @@ HTML
 };
 
 get '/run/:cmd_id' => sub {
-	my $cmd_id = param('cmd_id');
-	my $cmd = $cmds{$cmd_id} or return "Invalid $cmd_id";
+    my $cmd_id = param('cmd_id');
+    my $cmd = $cmds{$cmd_id} or return "Invalid $cmd_id";
 
-	my $stdout_fh    = File::Temp->new;
-	my $stderr_fh    = File::Temp->new;
-	my $stdout_fname = $stdout_fh->filename;
-	my $stderr_fname = $stderr_fh->filename;
-	$stdout_fh->unlink_on_destroy(0);
-	$stderr_fh->unlink_on_destroy(0);
-	$stdout_fh->close;
-	$stderr_fh->close;
+    my $stdout_fh    = File::Temp->new;
+    my $stderr_fh    = File::Temp->new;
+    my $stdout_fname = $stdout_fh->filename;
+    my $stderr_fname = $stderr_fh->filename;
+    $stdout_fh->unlink_on_destroy(0);
+    $stderr_fh->unlink_on_destroy(0);
+    $stdout_fh->close;
+    $stderr_fh->close;
 
-	my @cmd = ("$cmd >$stdout_fname 2>$stderr_fname");
+    my @cmd = ("$cmd >$stdout_fname 2>$stderr_fname");
 
-	if ( $^O eq 'MSWin32' ) {
+    if ( $^O eq 'MSWin32' ) {
 
-		# Windows OS
-		$pid = system( 1, @cmd );
-	}
-	else {
-		# Non-windows OS
-		$pid = fork();
-		if ( !$pid ) {
-			exec(@cmd);
-			exit;
-		}
-	}
-	
-	# Make sure the old result is dead
-	delete $results{$cmd_id};
+        # Windows OS
+        $pid = system( 1, @cmd );
+    }
+    else {
+        # Non-windows OS
+        $pid = fork();
+        if ( !$pid ) {
+            exec(@cmd);
+            exit;
+        }
+    }
 
-	my $timer;
-	$timer = AnyEvent->timer(
-		after    => 0,
-		interval => 0.5,
-		cb       => sub {
-			my $stdout = io($stdout_fname)->slurp;
-			my $stderr = io($stderr_fname)->slurp;
+    # Make sure the old result is dead
+    delete $results{$cmd_id};
 
-			# Update the process result record
-			$results{$cmd_id} = [ $pid, $stdout, $stderr ];
+    my $timer;
+    $timer = AnyEvent->timer(
+        after    => 0,
+        interval => 0.5,
+        cb       => sub {
+            my $stdout = io($stdout_fname)->slurp;
+            my $stderr = io($stderr_fname)->slurp;
 
-			my $running = kill 0, $pid;
-			# Cancel timer if process is dead
-			delete $timers{$timer} unless ($running);
-		}
-	);
-	$timers{$timer} = $timer;
+            # Update the process result record
+            $results{$cmd_id} = [ $pid, $stdout, $stderr ];
 
-	return "Running `$cmd`";
+            my $running = kill 0, $pid;
+
+            # Cancel timer if process is dead
+            delete $timers{$timer} unless ($running);
+        }
+    );
+    $timers{$timer} = $timer;
+
+    return "Running `$cmd`";
 
 };
 
 get '/result/:index' => sub {
-	my $index = param('index');
+    my $index = param('index');
 
-	return "Invalid index" unless defined $index;
+    return "Invalid index" unless defined $index;
 
-	my $result = $results{$index};
-	
-	return "No result for `$index`" unless defined $result;
-	return '<pre>' . $result->[1] . $result->[2] . '</pre>';
+    my $result = $results{$index};
+
+    return "No result for `$index`" unless defined $result;
+    return '<pre>' . $result->[1] . $result->[2] . '</pre>';
 };
 
 true;
