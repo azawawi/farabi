69c413379d55d4a726df843802617371708d6243
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2014-06-09 08:42:02 +0300

    Find files in module via MetaCPAN API now works

--- eg/find_all_files_in_module.pl
+++ eg/find_all_files_in_module.pl
@@ -1,37 +1,70 @@
 #!/usr/bin/env perl
 
 use Modern::Perl;
-use Data::Printer;
+use Method::Signatures;
 use ElasticSearch;
+use Data::Printer;
 
-sub es {
-    return ElasticSearch->new(
-        no_refresh => 1,
-        servers => 'api.metacpan.org',
-        #trace_calls => \*STDOUT,
-    );
+func find_files_in_module($module) {
+
+	sub es {
+		return ElasticSearch->new(
+			no_refresh => 1,
+			servers => 'api.metacpan.org',
+			#trace_calls => \*STDOUT,
+		);
+	}
+
+	my $latest = es()->search(
+		index  => 'v0',
+		type   => 'release',
+		fields => [ 'distribution', 'version' ],
+		size   => 3,
+		query  => {
+			filtered => {
+				query  => { match_all => {} },
+				filter => {
+					and => [
+						{ term => { 'release.status' => 'latest' } },
+						{   terms => {
+								'release.distribution' =>
+									[ $module ]
+							},
+						},
+					],
+				},
+			},
+		},
+	);
+
+	my @releases = map { $_->{fields} } @{ $latest->{hits}->{hits} };
+	die "Invalid release count: $#releases" if scalar @releases != 1;
+	my $release = $releases[0]->{distribution} . "-" . $releases[0]->{version};
+
+	my $files = es()->search(
+		index => 'v0',
+		type => 'file',
+		size => 5000,
+		query => {
+			filtered => {
+				query => { match_all => {} },
+				filter => {
+					bool => {
+						must => {
+							term => { 'file.release' => $release },
+						},
+						must_not => { term => { 'file.directory' => 'true' }, },
+					},
+				},
+			},
+		},
+	);
+
+	my @files = sort map { $_->{_source}->{path} } @{ $files->{hits}->{hits} };
+	return \@files;
 }
 
-#TODO get release name from http://api.metacpan.org/v0/release/Farabi
-
-my $files = es()->search(
-    index => 'v0',
-    type => 'file',
-    size => 300,
-    query => {
-        filtered => {
-            query => { match_all => {} },
-            filter => {
-                bool => {
-                    must => {
-                        term => { 'file.release' => 'Farabi-0.47' },
-                    },
-                    must_not => { term => { 'file.directory' => 'true' }, },
-                },
-            },
-        },
-    },
-);
-
-my @files = sort map { $_->{_source}->{path} } @{ $files->{hits}->{hits} };
-p @files;
+#p find_files_in_module('Farabi');
+#p find_files_in_module('Moose');
+#p find_files_in_module('Mojolicious');
+p find_files_in_module('Dancer');
