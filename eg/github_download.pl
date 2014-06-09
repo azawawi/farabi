cd470c9b984bea5da36b73322557a0cd9acf6168
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2014-06-09 17:25:09 +0300

    Add incomplete Github download example using Mojo::UserAgent for safekeeping

---
+++ eg/github_download.pl
@@ -0,0 +1,19 @@
+use Modern::Perl;
+use HTTP::Tiny;
+use Method::Signatures;
+use Mojo::UserAgent;
+use Data::Printer;
+
+func download_github_repo (Str $user, Str $repo) {
+	                          #if(-z '02packages.details.txt.gz') {
+	                          #}
+							  my $url = "https://github.com/$user/$repo/archive/master.zip";
+	my $response =
+		Mojo::UserAgent->new->max_redirects(1)->get($url)->res->body;
+	
+	$response;
+}
+
+my $archive = download_github_repo( 'azawawi', 'Farabi' );
+
+#p $archive;
