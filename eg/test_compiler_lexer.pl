a9869dfe7f9ef3e6887b7c1c27b0471ce8b6bdf8
Ahmad M. Zawawi <ahmad.zawawi@gmail.com>
2014-09-20 17:16:36 +0300

    A Compiler::Lexer experiment to get all the subroutines under @INC with their package name space prefix

---
+++ eg/test_compiler_lexer.pl
@@ -0,0 +1,55 @@
+use Modern::Perl;
+use Compiler::Lexer;
+use Data::Printer;
+use File::Find::Rule;
+use Path::Tiny;
+
+my @files = File::Find::Rule->file()
+	->name( '*.pm' )
+    ->in( @INC );
+
+#my @files = ('~/farabi/eg/helloworld.pl');
+
+for my $file_name (@files) {
+	my $script = path($file_name)->slurp;
+	my $tokens = Compiler::Lexer->new->tokenize($script);
+
+	my $i           = 0;
+	my @tokens      = @$tokens;
+	my $current_pkg = 'main';
+	do {
+		my $token = $tokens[$i];
+		if ( $token->name eq 'Package' ) {
+
+			# package Foo::Bar;
+
+			my $j        = $i + 1;
+			my $pkg_name = '';
+			while ($tokens[$j]->name eq 'Namespace'
+				or $tokens[$j]->name eq 'NamespaceResolver' )
+			{
+				$pkg_name .= $tokens[$j]->data;
+				$j++;
+			}
+			$current_pkg = $pkg_name;
+
+			say "--- $pkg_name";
+		}
+		elsif ( $token->name eq 'FunctionDecl' ) {
+
+			# sub foo { }
+			my $j = $i + 1;
+			if ( $tokens[ $j ]->name eq 'Function' ) {
+				my $sub_name = $tokens[$j]->data;
+				say
+				  "$current_pkg::$sub_name";
+			}
+		}
+
+		#p $token;
+		$i++;
+	} while ( $i < scalar @tokens );
+
+	say path($file_name)->basename;
+}
+
